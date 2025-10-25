692b53f4f1ac2fcef3f3aa3a853f94a5e405a0db
Kimsuro <ss01056489656@gmail.com>
2025-10-25 17:29:45 +0900

    feat(app): 자동차 경주 게임 전체 로직 통합

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { readAttemptCount, readCarNames } from './function/inputView.js';
+import { printRaceStatus, printResultHeader, printWinners } from './function/outputView.js';
+import { validateAttemptCount, validateCarNames } from './function/validator.js';
+import parseCarName from './function/parseCarName.js';
+import RacingGame from './models/RacingGame.js';
+
 class App {
-  async run() {}
+  async run() {
+    const carNameInput = await readCarNames();
+    const carNameArray = parseCarName(carNameInput);
+    validateCarNames(carNameArray);
+
+    const attemptInput = await readAttemptCount();
+    validateAttemptCount(attemptInput);
+    const attemptCount = Number(attemptInput);
+
+    const game = new RacingGame(carNameArray);
+
+    printResultHeader();
+
+    for (let i = 0; i < attemptCount; i += 1) {
+      game.playRound();
+      printRaceStatus(game.getCars());
+    }
+
+    const winners = game.getWinners();
+    printWinners(winners);
+  }
 }
 
 export default App;
