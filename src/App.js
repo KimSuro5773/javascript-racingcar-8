84015ce92efc25d4e486d60bb2a7bfd43b2f7061
Kimsuro <ss01056489656@gmail.com>
2025-10-27 15:17:52 +0900

    refactor(app): run 메서드를 단일 책임 원칙에 맞게 분리
    - run 메서드의 세부 로직을 5개의 private 메서드로 분리
    - 각 메서드가 하나의 명확한 책임만 수행하도록 개선

--- src/App.js
+++ src/App.js
@@ -6,23 +6,43 @@ import RacingGame from './models/RacingGame.js';
 
 class App {
   async run() {
+    const carNames = await this.#getValidCarNames();
+    const attemptCount = await this.#getValidAttemptCount();
+
+    const game = this.#createGame(carNames);
+
+    this.#playRounds(game, attemptCount);
+
+    this.#displayWinners(game);
+  }
+
+  async #getValidCarNames() {
     const carNameInput = await readCarNames();
-    const carNameArray = parseCarName(carNameInput);
-    validateCarNames(carNameArray);
+    const carNames = parseCarName(carNameInput);
+    validateCarNames(carNames);
+    return carNames;
+  }
 
-    const attemptInput = await readAttemptCount();
-    validateAttemptCount(attemptInput);
-    const attemptCount = Number(attemptInput);
+  async #getValidAttemptCount() {
+    const attemptCountInput = await readAttemptCount();
+    validateAttemptCount(attemptCountInput);
+    return Number(attemptCountInput);
+  }
 
-    const game = new RacingGame(carNameArray);
+  #createGame(carNames) {
+    return new RacingGame(carNames);
+  }
 
+  #playRounds(game, attemptCount) {
     printResultHeader();
 
     for (let i = 0; i < attemptCount; i += 1) {
       game.playRound();
       printRaceStatus(game.getCars());
     }
+  }
 
+  #displayWinners(game) {
     const winners = game.getWinners();
     printWinners(winners);
   }
